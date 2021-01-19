2e6f2337bf324f2c6f70e587392eff3c9dcee823
Nick Buraglio <nick@buraglio.com>
2021-01-18 19:46:40 -0600

    fix up links

---
+++ content/archive/chris/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  header( 'Location: http://www.google.com/search?hl=en&q=%22Chris+Buraglio%22' ) ;
+
+?>
+
+//<?php
+//$referer = $_SERVER['HTTP_REFERER'];
+//$domain_name = "www.qosbox.com";
+//if(strpos($referer, $domain_name)!== FALSE) {
+//	header("HTTP/1.1 301 Moved Permanently");
+//	header("Location: http://www.coprophage.net/");
+//	exit;
+//}
+
---
+++ content/archive/rebecca/gallery/embed.php
@@ -0,0 +1,4 @@
+<?php
+define('GALLERY_CONFIG_DIR', dirname(__FILE__));
+require('/a/services/www/buraglio.com/www/data/nick/gallery2/embed.php');
+?>
